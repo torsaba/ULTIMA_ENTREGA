743424ef3459a83f9dfd9813c5533a2f0829eb01
santi-esquerre <bymaxxeryt@gmail.com>
2023-08-09 16:18:24 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,31 +1,33 @@
-<<<<<<< Updated upstream
 document.addEventListener("DOMContentLoaded", () => {
-    const inicioSesion = document.getElementById("iniciar_sesion"); // submit
+  const inicioSesion = document.getElementById("iniciar_sesion"); // submit
 
-    inicioSesion.addEventListener("click", function(event) {
-        event.preventDefault(); // para que el submit no funciona default y recargue la pagina
-        
-        const email = document.querySelector('input[name="email"]').value; // guarda el email 
-        const password = document.querySelector('input[name="password"]').value; // guarda el password
+  inicioSesion.addEventListener("click", function (event) {
+    event.preventDefault(); // para que el submit no funciona default y recargue la pagina
 
-        if (email.includes("@")  && password) { // evalua que sea correcto (condicion que siempre se cumple)
-            window.location.href = "./index.html"; // redirecciona al index
-        } else {
-            const mensajeError = document.createElement("p"); // crea un mensaje de error
-            mensajeError.textContent = "Usuario o contraseña incorrecta. Intente otra vez.";
-            document.getElementById("formulario").appendChild(mensajeError); 
-            // esto es innecesario porque siempre da correcto el inicio
+    const email = document.querySelector('input[name="email"]').value; // guarda el email
+    const password = document.querySelector('input[name="password"]').value; // guarda el password
 
-            setTimeout(function() { // para que el mensaje de error desaparezca
-                mensajeError.remove();
-            }, 2000); 
-        }
-    });
+    if (email.includes("@") && password) {
+      // evalua que sea correcto (condicion que siempre se cumple)
+      window.location.href = "./index.html"; // redirecciona al index
+    } else {
+      const mensajeError = document.createElement("p"); // crea un mensaje de error
+      mensajeError.textContent =
+        "Usuario o contraseña incorrecta. Intente otra vez.";
+      document.getElementById("formulario").appendChild(mensajeError);
+      // esto es innecesario porque siempre da correcto el inicio
+
+      setTimeout(function () {
+        // para que el mensaje de error desaparezca
+        mensajeError.remove();
+      }, 2000);
+    }
+  });
 });
 
 /* faltaría que guarde el "Recordarme", y si eso pasa, que el usuario al entrar al login,
 lo rediriga a la pagina, no se como hacerlo */
-=======
+
 regBtn.addEventListener("click", function loginUser() {
   // *PENDIENTE* Validar login
 
@@ -33,4 +35,3 @@ regBtn.addEventListener("click", function loginUser() {
   localStorage.setItem("isLoggedIn", "true");
   window.location.href = "index.html"; // Ir a la página principal
 });
->>>>>>> Stashed changes
