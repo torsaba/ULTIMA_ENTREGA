017cb6f2e95c5b910e8d70aa598de039a8a34f2d
lucasmiraballes <141080946+lucasmiraballes@users.noreply.github.com>
2023-10-07 20:18:58 -0300

    Branch lucas (#37)

---
+++ js/cart.js
@@ -0,0 +1,44 @@
+async function cargarProductos() { //funcion que agarra los datos del json unicamente y los muestra
+    try {
+        const response = await fetch("https://japceibal.github.io/emercado-api/user_cart/25801.json");
+
+        if (!response.ok) {
+            throw new Error('No se pudo completar la solicitud.');
+        }
+
+        // si no hay errores en la solicitud se procede con:
+        const data = await response.json(); // la info del .json
+        const cartBody = document.getElementById("cart-products"); // toma la tabla donde van a ir los productos
+
+        data.articles.forEach(article => {
+            const newProduct = document.createElement("tr"); // se crea la celda para cada producto
+
+            // lo que se crea, esta todo, incluido el boton de eliminar la celda
+            newProduct.innerHTML = ` 
+                <td><img src="${article.image}" alt="${article.name}" width="150"></td>
+                <td>${article.name}</td>
+                <td>${article.unitCost} ${article.currency}</td>
+                <td><input class="form-control" type="number" min="0" value="${article.count}" 
+                id="quantity-${article.id}"></td>
+                <td class="text-primary" id="subtotal-${article.id}">${article.unitCost * article.count} ${article.currency}</td>
+                <td><button class="btn btn-danger" onclick="eliminarProducto(${article.id})">Eliminar</button></td>
+            `;
+
+            cartBody.appendChild(newProduct); // coloca la celda del producto en html mostrandose en la pagina
+        });
+    } catch (error) {
+        console.error('Error en la solicitud:', error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", cargarProductos); // se ejecuta la funcion con DOM
+
+
+
+
+function eliminarProducto(productId) { // esta funcion elimina la celda que eligas 
+    const eliminarProd = document.getElementById(`quantity-${productId}`).closest("tr"); // toma la celda de dicho producto
+    if (eliminarProd) {
+        eliminarProd.remove(); // verificacion siempre true, la elimina si damos click al boton
+    }
+}
