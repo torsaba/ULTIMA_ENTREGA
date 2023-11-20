657b2de728e804bb5a5a21e87600f8c561458eef
torsaba <141196826+torsaba@users.noreply.github.com>
2023-11-20 11:40:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ app.get("/", (req, res) => {
   app.get("/cats", (req, res) => {
     res.json(cats);
   });
-app.get("/cats_products/:id", (req, res) =>
-{
+
   app.get("/cats_products/:id", (req, res) => {
     const id = req.params.id;
     const filePath = path.join(__dirname, "json", "cats_products", `${id}.json`);
@@ -30,7 +29,36 @@ app.get("/cats_products/:id", (req, res) =>
       }
     });
   });
+  app.get("/products/:id", (req, res) => {
+    const id = req.params.id;
+    const moduloPath = path.join(__dirname, "json", "products", `${id}.json`);
+  
+    fs.readFile(moduloPath, "utf-8", (error, data) => {
+      if (!error) {
+       
+        const productos = JSON.parse(data);
+        res.json(productos);
+      } else {
+        return res.status(404).json({ error: "Producto no encontrado" });
+      }
+    });
   });
+  app.get("/products_comments/:id", (req, res) => {
+    const id = req.params.id;
+    const moduloDosPath = path.join(__dirname, "json", "products_comments", `${id}.json`);
+  
+    fs.readFile(moduloDosPath, "utf-8", (error, data) => {
+      if (!error) {
+       
+        const comentarios = JSON.parse(data);
+        res.json(comentarios);
+      } else {
+        return res.status(404).json({ error: "Este producto no tiene comentarios" });
+      }
+    });
+  });
+  
+  
  
 
   app.listen(puerto, () => {
