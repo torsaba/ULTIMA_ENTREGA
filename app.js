27a455ae2d4e690a591f2836026d232d774cdf94
lucasmiraballes <lucas20miraballes@gmail.com>
2023-11-22 08:45:05 -0300

    comentado

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const express = require("express"); //Utilizando el framework
-const fs = require("fs"); //Se importa el módulo 'fs' (File System) para trabajar con el sistema de archivos
+// Instancias y librerias
+
+const express = require("express"); // Utilizando el framework
+const fs = require("fs"); // Se importa el módulo 'fs' (File System) para trabajar con el sistema de archivos
 const path = require("path"); // Se importa el módulo 'path' para manipular rutas de archivos y directorios
 const app = express(); // Instancia de express
 const jwt = require("jsonwebtoken"); // Importa la librería jsonwebtoken
-const cors = require("cors");
-const mariadb = require("mariadb");
+const cors = require("cors"); // Cors para las autenticaciones (creo que no se usa porque se termina usando jwt)
+const mariadb = require("mariadb"); // Instancia de mariaDB
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json()); // Se inicializa express
+app.use(cors()); // Se inicializa cors
 
 const puerto = 3000; // Indico puerto al que va a escuchar
-let cats = require("./json/./cats/cat.json"); //Utilizo la ruta al archivo JSON
+let cats = require("./json/./cats/cat.json"); // Utilizo la ruta al archivo JSON
 
+/* Estos get funcionan con los archivos .json y muestran la info del json en la web
+ (formato .json, no formato web) */
 app.get("/", (req, res) => {
   res.send("<h1>Bienvenidos al servidor</h1>");
 });
@@ -69,6 +73,7 @@ app.get("/products_comments/:id", (req, res) => {
   });
 });
 
+// Estos get funcionan para obtener las paginas web .html con el formato que se utiliza en la web
 app.get("/:filename.html", (req, res) => {
   const filename = req.params.filename;
   const filePath = path.join(__dirname, `${filename}.html`);
@@ -79,6 +84,7 @@ app.get("/:filename.html", (req, res) => {
   }
 });
 
+// Este get entra en la carpeta json, y en sus carpetas respectivas para obtener los .json (todos)
 app.get("/json/:subfolder/:filename?", (req, res) => {
   const subfolder = req.params.subfolder;
   const filename = req.params.filename;
@@ -97,17 +103,18 @@ app.get("/json/:subfolder/:filename?", (req, res) => {
   }
 });
 
-const SECRET_KEY = "clave" // Middleware de autorización
+// JWT, secretKey y authorizeMiddleware para la autentificacion de login y el ingreso a la web
+const SECRET_KEY = "clave"; // Middleware de autorización
 const authorizeMiddleware = (req, res, next) => {
-  const token = req.headers['access-token'];
+  const token = req.headers["access-token"];
 
   if (!token) {
-    return res.status(401).json({ error: 'Token no proporcionado' });
+    return res.status(401).json({ error: "Token no proporcionado" });
   }
 
   jwt.verify(token, SECRET_KEY, (err, decoded) => {
     if (err) {
-      return res.status(401).json({ error: 'Token no válido' });
+      return res.status(401).json({ error: "Token no válido" });
     }
 
     req.user = decoded.user;
@@ -115,49 +122,61 @@ const authorizeMiddleware = (req, res, next) => {
   });
 };
 
-app.post('/login', (req, res) => {
+// Post para la autentificacion del usuario
+app.post("/login", (req, res) => {
   const { user, password } = req.body;
-  
-  if (user === 'admin@admin.com' && password === 'admin123') {
+
+  if (user === "admin@admin.com" && password === "admin123") {
     const token = jwt.sign({ user }, SECRET_KEY);
     res.json({ token, user });
   } else {
-    res.status(401).json({ error: 'Credenciales incorrectas' });
+    res.status(401).json({ error: "Credenciales incorrectas" });
   }
 });
 
-app.get('/cart', (req, res) => {
+// Get que permite ingresar con la aut. del usuario
+app.get("/cart", (req, res) => {
   const user = req.user;
 
   // Lógica para la ruta /cart aquí
 
-  res.json({ message: 'Ruta /cart accesible solo para usuarios autenticados', user });
+  res.json({
+    message: "Ruta /cart accesible solo para usuarios autenticados",
+    user,
+  });
 });
 
-
+// Base de datos en mariaDB, si no la tenemos no funciona ingresar los items del carrito en la bbdd
 global.pool = mariadb.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '12345',
-  database: 'carrito',
-  connectionLimit: 50
- });
- 
- app.post('/guardarDatos', async (req, res) => {
-  try {
-     const { name, img, cost, moneda } = req.body;
- 
-     const conn = await pool.getConnection();
-     await conn.query('INSERT INTO carttest (name, img, cost, moneda) VALUES (?, ?, ?, ?)', [name, img, cost, moneda]);
-     conn.release();
- 
-     res.status(200).json({ message: 'Datos guardados correctamente' });
-  } catch (error) {
-     console.error('Error al guardar datos:', error);
-     res.status(500).json({ error: 'Hubo un problema al guardar los datos' });
-  }
- });
+  host: "localhost",
+  user: "root",
+  password: "12345",
+  database: "carrito",
+  connectionLimit: 50,
+});
 
+/* Este post envía cada item del carrito a la base de datos y trabaja con los datos del objeto
+ (se modificó product-info.js para las propiedades del objeto) */
+app.post("/guardarDatos", async (req, res) => {
+  try {
+    const { name, img, cost, moneda } = req.body;
+
+    const conn = await pool.getConnection();
+    await conn.query(
+      "INSERT INTO carttest (name, img, cost, moneda) VALUES (?, ?, ?, ?)",
+      [name, img, cost, moneda]
+    );
+    conn.release();
+
+    res.status(200).json({ message: "Datos guardados correctamente" });
+  } catch (error) {
+    console.error("Error al guardar datos:", error);
+    res.status(500).json({ error: "Hubo un problema al guardar los datos" });
+  }
+});
+
+/* Los .use con authorizeMiddleware validan la aut. del usuario,
+ y los express.static trabaja con los recursos estáticos para darle FORMATO WEB a las paginas */
 app.use("/index", authorizeMiddleware);
 app.use("/cart", authorizeMiddleware);
 app.use("/js", express.static(path.join(__dirname, "js")));
@@ -166,6 +185,7 @@ app.use("/img", express.static(path.join(__dirname, "img")));
 app.use("/webfonts", express.static(path.join(__dirname, "webfonts")));
 app.use("/json", express.static(path.join(__dirname, "json")));
 
+// Escucha el puerto e inicializa el sv- <npm run dev>
 app.listen(puerto, () => {
   console.log(`Servidor escuchando en el puerto ${puerto}`);
 });
